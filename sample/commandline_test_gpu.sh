a6005c673c0db9364270f1c12e552f8b5f3b0d44
mohammad ali hossien beigi <mohammadalihossienbeigi1997@gmail.com>
2025-01-22 11:48:36 +0330

    fix: disable activate `pytorch3drugcell` env

--- sample/commandline_test_gpu.sh
+++ sample/commandline_test_gpu.sh
@@ -22,6 +22,6 @@ fi
 mkdir $resultdir
 mkdir $hiddendir
 
-source activate pytorch3drugcell
+#source activate pytorch3drugcell
 
 python -u ../code/predict_drugcell.py -gene2id $gene2idfile -cell2id $cell2idfile -drug2id $drug2idfile -genotype $mutationfile -fingerprint $drugfile -hidden $hiddendir -result $resultdir -predict $inputdir/drugcell_test.txt -load $modelfile -cuda $cudaid > test_sample.log
